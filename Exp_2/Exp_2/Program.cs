07324c4df060c260da9062388eaf9513cb4ea90a
Mrudula Waghmode <mrudulawaghmode11@gmail.com>
2026-03-07 15:11:27 +0530

    exp_02

---
+++ Exp_2/Exp_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp_2
+{
+    internal class Program
+    {   class Student
+        {
+            private int id;                 //private data members =>encapsulation 
+            private string name;            
+
+            public void SetStudent(int sid,string sname)        //set method
+            {
+                id = sid;
+                name = sname;
+            }
+            public void DisplayStudent()                    //display method
+            {
+                Console.WriteLine("Student id : " + id);
+                Console.WriteLine("Student name : " + name);
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Student stu = new Student();                    //creating object of student class
+            stu.SetStudent(137,"Mrudula");                  //accessing methods using objects
+            stu.DisplayStudent();                           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exp_2/OopsConcepts/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts
+{
+    internal class Program
+    {   class Person
+        {
+            private string name;
+            public void SetName(string pname)
+            {
+                name = pname;
+            }
+            public void DisplayName()
+            {
+                Console.WriteLine("person name : " + name);
+            }
+        }
+        //inheritance : derived class
+        class Student : Person
+        {
+            public void ShowDetails(int id)                     //polymorphism
+            {
+                Console.WriteLine("Student Id : " + id);
+            }
+            public void ShowDetails(int id,string course)
+            {
+                Console.WriteLine("Student Id : " + id);
+                Console.WriteLine("Student course : " + course);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            Console.WriteLine("set method=>set name");
+            s1.SetName("Mrudula");
+            Console.WriteLine("Display method");
+            s1.DisplayName();
+            //derived class methods accessed using objects
+            Console.WriteLine("show method with one parameter");
+            s1.ShowDetails(137);
+            Console.WriteLine("show method with two parameters");
+            s1.ShowDetails(137, "Mrudula");
+            Console.ReadLine();
+
+        }
+    }
+}
