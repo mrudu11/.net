6fd7435b658333e64290ec18e09a21cd9785f31d
Mrudula Waghmode <mrudulawaghmode11@gmail.com>
2026-04-23 11:10:25 +0530

    exp_8

---
+++ Exp_8/StudentCRUDApp/Models/Students.cs
@@ -0,0 +1,8 @@
+public class Student        //table name
+{
+    public string name{get;set;}=string.Empty;
+    public string age{get;set;}=string.Empty;
+    public string course{get;set;}=string.Empty;
+    public int id{get;set;};
+
+}
